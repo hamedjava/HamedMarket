46f781e7e7996cafeaa70773f47aa5145c4646a6
hamedjava <hamedpouyanfar1987@gmail.com>
2022-06-29 02:03:37 +0430

    complete design layouts

--- app/src/main/java/com/example/hamedmarket/MainActivity.java
+++ app/src/main/java/com/example/hamedmarket/MainActivity.java
@@ -1,14 +1,43 @@
 package com.example.hamedmarket;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.material.appbar.MaterialToolbar;
+import com.google.android.material.navigation.NavigationView;
+
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener{
+
+    DrawerLayout drawer;
+    ActionBarDrawerToggle toggle;
+    NavigationView nav_view;
+    MaterialToolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        setupViews();
+        setSupportActionBar(toolbar);
+        toggle = new ActionBarDrawerToggle(this,drawer,toolbar,R.string.nav_open,R.string.nav_close);
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
+        nav_view.setNavigationItemSelectedListener(this);
+    }
+    public void setupViews(){
+        toolbar = (MaterialToolbar) findViewById(R.id.custom_toolbar);
+        drawer = (DrawerLayout) findViewById(R.id.drawer);
+        nav_view = (NavigationView) findViewById(R.id.nav_view);
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
+        return true;
     }
 }
